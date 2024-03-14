2e40b770e2829e96e80333e5dd1b33bab0e03c82
ionescuea <ionescuea@yahoo.com>
2024-03-14 20:40:42 +0000

    Add components to the structure

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navigation() {
+  return (
+    <div className="container">
+      <h5>Navigation: Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+    </div>
+  );
+}
+
+export default Navigation;
