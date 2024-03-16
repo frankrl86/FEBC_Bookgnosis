4b56efbcc4ed72917eef1b71a8ee3e5674754c6e
Paul <paulasquith150@gmail.com>
2024-03-16 23:47:14 +0000

    updated footer left paragraph alignment

--- bookgnosis/src/footer.jsx
+++ bookgnosis/src/footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           <div className="col-md-6">
             <div className="bookgnosis-block d-flex flex-column align-items-left mb-4">
               <h4 className="text-muted mb-3">Bookgnosis</h4>
-              <p className="text-muted mb-4">Welcome to our book reading app.</p>
+              <p className="text-muted mb-4" align="left">Welcome to our book reading app.</p>
               <div className="social-icons d-flex flex-row">
                 <a href="https://www.facebook.com" className="me-3">
                   <img src={facebookIcon} alt="Facebook" className="img-fluid smlicon" />
